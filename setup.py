4aabd205d3d6f5a130c6df4ab8ebee49a46b06aa
Soyadrul <118212567+Soyadrul@users.noreply.github.com>
2025-05-28 21:02:05 +0200

    feat: Added CV python paths

--- setup.py
+++ setup.py
@@ -7,13 +7,19 @@ package_name = 'image_saver'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    #packages=find_packages(exclude=['test']),
+    packages=['image_saver', 'image_saver.open_waters', 'image_saver.open_waters.wasr', 'image_saver.open_waters.weights', 'launch', 'resource'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
+    package_data={
+        # Include all files in weights folder
+        package_name: ['image_saver/open_waters/weights/*.pth'],
+    },
+    include_package_data=True,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='pi',
